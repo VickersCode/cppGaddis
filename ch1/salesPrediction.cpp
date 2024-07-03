0e9099c2a6c37744c15c33ab089d2c97b449600e
Tyler Vickers <tyler31vickers@gmail.com>
2024-07-03 12:56:49 -0700

    add ch1 prob2

---
+++ ch1/salesPrediction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    float sales = 4600000;
+    float TAX = 0.62;
+
+    float total = sales * TAX;
+
+    cout << "Total revenue is: $" << total;
+
+    return 0;
+}
