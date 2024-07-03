99e5d73da632769a620159b03c56a7dc87d0474d
Tyler Vickers <tyler31vickers@gmail.com>
2024-07-03 12:44:06 -0700

    complete ch1 prob1

---
+++ ch1/sumOfTwoNum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int var1 = 62, 
+        var2 = 99;
+
+    int total = var1 + var2;
+
+    cout << "The total is: " << total;
+
+    return 0;
+}
