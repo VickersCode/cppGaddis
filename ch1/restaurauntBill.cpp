3b3a79a06e1d8d09d6b2fc517b021c788a2862bc
VickersCode <tyler31vickers@gmail.com>
2024-07-05 10:00:41 -0700

    add boilerplate for ch1 prob4

---
+++ ch1/restaurauntBill.cpp
@@ -0,0 +1,12 @@
+/*
+Write a program that computes the tax and tip on a restauraunt bill for a patron with a $44.50 meal charge. The tax should be 6.75% percent of the meal cost. The tip should be 15% of the total after adding the tax. Display the meal cost, tax amount, tip amount, and total bill on the screen.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
