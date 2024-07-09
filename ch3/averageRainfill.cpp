ff8839d94ec5fc40308618cae08fa656c6301d25
Tyler Vickers <tyler31vickers@gmail.com>
2024-07-09 16:14:00 -0700

    update ch3 prob7

--- ch3/averageRainfill.cpp
+++ ch3/averageRainfill.cpp
@@ -5,12 +5,33 @@ The average monthly rainfall for June, July, and August was 6.72 inches.
 */
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
 int main() {
 
+    string month1,
+           month2,
+           month3;
 
+    double month1Rain,
+           month2Rain,
+           month3Rain,
+           monthAvg;
+
+    cout << "Finding 3-month average \n";
+    cout << "Enter the first and the amount of rain (in inches) fallen that month: ";
+    cin >> month1 >> month1Rain;
+    cout << "Enter the second and the amount of rain (in inches) fallen that month: ";
+    cin >> month2 >> month2Rain;
+    cout << "Enter the third and the amount of rain (in inches) fallen that month: ";
+    cin >> month3 >> month3Rain;
+
+    monthAvg = (month1Rain + month2Rain + month3Rain) / 3;
+
+    cout << fixed << setprecision(2);
+    cout << "Your average rainfall for " << month1 << " " << month2 << " and " << month3 << " is: " << monthAvg << " inches"; 
 
     return 0;
 }
