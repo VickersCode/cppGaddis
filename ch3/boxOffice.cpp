fb314d54bb5b8ff0cfcd16b08baa464aad42d398
Tyler Vickers <tyler31vickers@gmail.com>
2024-07-09 16:21:46 -0700

    add boilerplate for ch3 prob8

---
+++ ch3/boxOffice.cpp
@@ -0,0 +1,22 @@
+/*
+A movie theater only keeps a percentage of the revenue earned from ticket sales. The remainder goes to the distributor. Write a program that calculates a theater's gross and net box office profit for a night. The program should ask for the name of the movie, and how many adult and child tickets were sold. (The price of an adult ticket is $6.00 and a childs ticket is $3.00) It should display a report similar to the following:
+
+    Movie Name:                 "Wheels of Fury"
+    Adult tickets sold:         382
+    Child Tickets sold:         127
+    Gross Box Office Profit:    $ 2673.00
+    Amount Paid to Distr:      -$ 3138.40
+    Net Box Office Profit:      $  534.60
+
+Assume the theater keeps 20% of the gross box office profit. 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    
+
+    return 0;
+}
