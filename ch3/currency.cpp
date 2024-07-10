e997cdc8132d8c3c76388d5f1d19c98e828ca68b
Tyler Vickers <tyler31vickers@gmail.com>
2024-07-10 16:46:17 -0700

    add boilerplate for ch3 prob12

---
+++ ch3/currency.cpp
@@ -0,0 +1,13 @@
+/*
+Write a program that will convert U.S. dollar amounts to Japanese yen and to euros, storing the conversion factors in constants. To get the most up-to-date exchange rates, search the Internet using the term "currency exchange rate" or "currency converter".
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
