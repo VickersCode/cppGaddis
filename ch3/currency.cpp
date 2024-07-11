c2ed2a532fc13fc5c35d3780e12a47107cd4cbf8
Tyler Vickers <tyler31vickers@gmail.com>
2024-07-11 08:28:36 -0700

    update ch3 prob12

--- ch3/currency.cpp
+++ ch3/currency.cpp
@@ -3,10 +3,27 @@ Write a program that will convert U.S. dollar amounts to Japanese yen and to eur
 */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
 
+    const double US_TO_YEN = 158.56,
+                 US_TO_EURO = 0.92;
+
+    double usDollars,
+           yenConversion,
+           euroConversion;
+
+    cout << "Enter the amount of dollars to convert: \n";
+    cin >> usDollars;
+
+    yenConversion = usDollars * US_TO_YEN;
+    euroConversion = usDollars * US_TO_EURO;
+
+    cout << fixed << setprecision(2);
+    cout << "Yen: " << yenConversion << endl;
+    cout << "Euros: " << euroConversion << endl;
 
 
     return 0;
