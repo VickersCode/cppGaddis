8bcc3628da1b8c4840cd463543d668f9e98057ac
VickersCode <tyler31vickers@gmail.com>
2024-07-12 06:38:30 -0700

    add boilerplate ch3 prob17

---
+++ ch3/interestEarned.cpp
@@ -0,0 +1,27 @@
+/*
+Assuming there are no deposits other than the original investment, the balance in a savings account after one year may be calculated as 
+
+                        Amount = Principal * (1 + Rate/T)^T
+
+where Principal is the balance in the account, Rate is the annual interest rate, and T is the number of times the interest is compunded during a year. (e.g., T is 4 if the interest is compounded quarterly.)
+
+Write a program that asks for the principal, the interest rate, and the number of times the interest is compounded. It should display a report similar to the following:
+
+                Interest Rate:           4.25%
+                Times Compounded:          12
+                Principal:          $ 1000.00
+                Interest:           $   43.33
+                Final Balance:      $ 1043.33
+
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
