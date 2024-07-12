ae9a4549d2ce603adca03c8c8174b4d1101ed5f1
VickersCode <tyler31vickers@gmail.com>
2024-07-11 18:35:10 -0700

    add boilerplate ch3 prob15

---
+++ ch3/seniorPropertyTax.cpp
@@ -0,0 +1,14 @@
+/*
+Madison County provides a $5000 homeowner exemption for its senior citizens. For example, if their house is valued at $158,000 its assessed value would be $94,800, as explained in propertyTax.cpp. However, they would only pay tax on $89,800. At last year's tax rate of $2.64 for each $100 of assessed value, their property tax would be $2370.72. In addition to the tax break, senior citizens are allowed to pay their property tax in 4 equal payments. The quarterly payment due on this property would be $592.68. Write a program that asks the user to input the actual value of a piece of property and the current tax rate for each $100 of assessed value. the program should then calculate and report how much annual property tax a senior homeowner will be charged for this property and what their quarterly tax bill will be. 
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main () {
+
+
+
+    return 0;
+}
